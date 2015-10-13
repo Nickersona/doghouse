5941d06f8d4685d5271fb51d08316e72d95f41c3
andrew nickerson <Andrew@andrews-MacBook-Pro.local>
2015-10-12 18:38:05 -0700

    updated to scraper to structure scraped data and export a json file of the request

--- scraper.js
+++ scraper.js
@@ -4,11 +4,10 @@ var fs = require('fs'),
 
 
 var DataPoint = function(name){
-	this.name = "";
-	var extractionMethods = {};
+	this.name = name;
+	this.extractionMethods = {};
 
 	this.addExtractionMethod = function(methodPage, methodFunction){
-		console.log(methodFunction);
 		this.extractionMethods[methodPage] = methodFunction;
 	}
 
@@ -22,30 +21,65 @@ var DataPoint = function(name){
 var PageScraper = function(url, callback){
 	this.baseUrl = url;
 	
-
+	this.callback = callback;
 	this.stats = {
+		searchUrl: url,
 		count: 0,
 		pages: 0
+
 	}
-	this.data = [];
+	this.data = {};
 	this.allFinished = false;
 	this.dataPoints = [];
 
+	//Describe the data points to extract and how to extract them from the listing domNode
 	var priceDataPoint = new DataPoint('price');
-	priceDataPoint.addExtractionMethod('overview', function(domNode){
-		return $(domNode).find('span.price').text();	
+	priceDataPoint.addExtractionMethod('overview', function($domNode){
+		return parseInt($domNode.find('span.price').text().substr(1));
 	});
 
-	this.dataPoints.push(priceDataPoint);
+	var listingName = new DataPoint('ListingName');
+	listingName.addExtractionMethod('overview', function($domNode){
+		return $domNode.find('a.hdrlnk').text();	
+	});
 
-	var tryCallback = function(){
-		if(this.allFinished){
-			callback(this.stats, this.data);
-		}
+	var location = new DataPoint('location');
+	location.addExtractionMethod('overview', function($domNode){
+		return $domNode.find('a.hdrlnk').text();
+	});
+
+	var listingHref = new DataPoint('href');
+	listingHref.addExtractionMethod('overview', function($domNode){
+		return 'http://vancouver.craigslist.ca' + $domNode.find('a.hdrlnk').attr('href');	
+	});
+
+	var listingDate = new DataPoint('date');
+	listingDate.addExtractionMethod('overview', function($domNode){
+		return Date.parse($domNode.find('time').attr('datetime'));
+	});
+
+	var sqft = new DataPoint('sqft');
+	sqft.addExtractionMethod('overview', function($domNode){
+		var string = $domNode.find('span.housing').text();
+		match = string.match(/([0-9]+)ft2/);
+		return (match)? parseInt(match[1]) : "";
+	});
+
+	this.dataPoints.push(sqft);
+	this.dataPoints.push(listingDate);
+	this.dataPoints.push(priceDataPoint);
+	this.dataPoints.push(listingName);
+	this.dataPoints.push(listingHref);
+
+	
+	this.crawlPage(this.baseUrl, this.dataPoints);
+	
+}
+
+PageScraper.prototype.tryCallback = function(){
+	if(this.allFinished){
+		this.callback(this.stats, this.data);
 	}
-	
-	this.crawlPage(this.baseUrl, dataPoints);
-	
 }
 
 PageScraper.prototype.crawlPage = function(url, dataPoints){
@@ -54,20 +88,33 @@ PageScraper.prototype.crawlPage = function(url, dataPoints){
 		if (!error && response.statusCode == 200){
 			self.stats.pages++;
 			var $ = cheerio.load(html);
-			$('span.price').each(function(i, element){
+
+			//Iterate through all the listings, and run every dataPoint overview extraction method agains the listing element
+			$('.content > p.row').each(function(i, listingEl){
+				var listingData = {};
 				self.stats.count++;
-				console.log($(element).text());
+
+				var id = parseInt($(listingEl).data('pid'));
+
+				//runs all the dataPoint extraction methods against the listing Node
+				for (var i = dataPoints.length - 1; i >= 0; i--) {
+					listingData[dataPoints[i].name] = dataPoints[i].getExtractionMethod('overview')($(listingEl));
+				};
+
+				self.data[id] = listingData;
 			});
 
+			self.stats.listedTotal = $('.totallink').text();
 
+			//If it's the last page run the callback otherwise generate the next pages url and crawl it
 			if(!$('.paginator.buttongroup').hasClass('lastpage')) {
 				url = advancePageUrl(url);
-				self.crawlPage(url);
+				self.crawlPage(url, dataPoints);
 				
 				console.log('Next Page >>');
 			}else{
-				this.allFinished = true;
-				tryCallback();
+				self.allFinished = true;
+				self.tryCallback();
 			}
 		}
 	});
@@ -87,17 +134,50 @@ function advancePageUrl(url){
 }
 
 
-var url = "http://vancouver.craigslist.ca/search/hhh?sort=rel&pets_dog=1&query=vancouver+apartment"
-url = advancePageUrl(url)
-url = advancePageUrl(url)
-url = advancePageUrl(url)
+var url = "http://vancouver.craigslist.ca/search/apa?pets_dog=0&amp;query=vancouver%20apartment&amp;sort=rel"
 
-var scraper = new PageScraper(url, function(stats){
+
+
+var scraper = new PageScraper(url, function(stats, data){
 	console.log(stats);
+
+	var avgPrice = calculateAverage(data);
+
+	var DataSet = {"stats" : stats, "data": data}
+
+
+
+
+	fs.writeFile('dataSet.json', JSON.stringify(DataSet, null, 4), function(err) {
+	    if(err) {
+	      console.log(err);
+	    }
+	}); 
 });
 
 
 
+function calculateAverage(dataSet){
+	var total = 0;
+	var dataKeys = Object.keys(dataSet);
+	var actualDataCount = dataKeys.length;
+	for (var i = dataKeys.length - 1; i >= 0; i--) {
+		if( isValidData(dataSet[dataKeys[i]].price) ){
+			total += dataSet[dataKeys[i]].price;
+		}else{
+			actualDataCount--;
+		}
+	};
+	console.log(actualDataCount);
+	console.log(total/actualDataCount);
+};
 
+function isValidData (datum){
+	return typeof datum === 'number' 
+	&& !isNaN(datum)
+	&& datum < 10000
+	&& datum > 100
+
+}
 
 
